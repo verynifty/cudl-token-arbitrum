a7704f6d42de41e35b7fcbc30cac76816d1fa838
Adam H <surfcoderepeat@protonmail.com>
2021-09-20 09:30:28 -0600

    make token compatible with bridge:

--- contracts/Cudl.sol
+++ contracts/Cudl.sol
@@ -4,4 +4,60 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract Cudl is ERC20PresetMinterPauser("CUDL", "CUDL") {}
+interface IArbToken {
+    /**
+     * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeMint(address account, uint256 amount) external;
+
+    /**
+     * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeBurn(address account, uint256 amount) external;
+
+    /**
+     * @return address of layer 1 token
+     */
+    function l1Address() external view returns (address);
+}
+
+contract Cudl is IArbToken, ERC20PresetMinterPauser("CUDL", "CUDL") {
+    address public l2Gateway;
+    address public override l1Address;
+
+    modifier onlyGateway() {
+        require(msg.sender == l2Gateway, "ONLY_GATEWAY");
+        _;
+    }
+
+    constructor(address l2Gateway_, address l1Counterpart_) {
+        require(l2Gateway_ != address(0), "INVALID_GATEWAY");
+        require(l2Gateway == address(0), "ALREADY_INIT");
+        l2Gateway = l2Gateway_;
+        l1Address = l1Counterpart_;
+    }
+
+    function bridgeMint(address account, uint256 amount)
+        external
+        virtual
+        override
+        onlyGateway
+    {
+        _mint(account, amount);
+    }
+
+    /**
+     * @notice Burn tokens on L2.
+     * @dev only the token bridge can call this
+     * @param account owner of tokens
+     * @param amount amount of tokens burnt
+     */
+    function bridgeBurn(address account, uint256 amount)
+        external
+        virtual
+        override
+        onlyGateway
+    {
+        _burn(account, amount);
+    }
+}
