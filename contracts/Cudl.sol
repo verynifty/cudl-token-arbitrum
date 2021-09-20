e25cc982fe35823d85c1c708244800c2025ff828
Adam H <surfcoderepeat@protonmail.com>
2021-09-20 16:52:39 -0600

    waffle way

--- contracts/Cudl.sol
+++ contracts/Cudl.sol
@@ -4,10 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 interface IArbToken {
     /**
@@ -26,12 +23,7 @@ interface IArbToken {
     function l1Address() external view returns (address);
 }
 
-contract Cudl is
-    IArbToken,
-    ERC20PresetMinterPauser("CUDL", "CUDL"),
-    IERC20Permit,
-    EIP712
-{
+contract Cudl is IArbToken, ERC20PresetMinterPauser, ERC20Permit {
     address public l2Gateway;
     address public override l1Address;
 
@@ -41,12 +33,21 @@ contract Cudl is
     }
 
     constructor(address l2Gateway_, address l1Counterpart_)
-        EIP712("CUDL", "1")
+        ERC20PresetMinterPauser("CUDL", "CUDL")
+        ERC20Permit("CUDL")
     {
         require(l2Gateway_ != address(0), "INVALID_GATEWAY");
         require(l2Gateway == address(0), "ALREADY_INIT");
         l2Gateway = l2Gateway_;
         l1Address = l1Counterpart_;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20, ERC20PresetMinterPauser) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 
     function bridgeMint(address account, uint256 amount)
@@ -71,92 +72,5 @@ contract Cudl is
         onlyGateway
     {
         _burn(account, amount);
-    }
-
-    // PERMIT
-
-    using Counters for Counters.Counter;
-
-    mapping(address => Counters.Counter) private _nonces;
-
-    // solhint-disable-next-line var-name-mixedcase
-    bytes32 private immutable _PERMIT_TYPEHASH =
-        keccak256(
-            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-        );
-
-    /**
-     * @dev Initializes the {EIP712} domain separator using the `name` parameter, and setting `version` to `"1"`.
-     *
-     * It's a good idea to use the same `name` that is defined as the ERC20 token name.
-     */
-
-    /**
-     * @dev See {IERC20Permit-permit}.
-     */
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) public virtual override {
-        require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
-
-        bytes32 structHash = keccak256(
-            abi.encode(
-                _PERMIT_TYPEHASH,
-                owner,
-                spender,
-                value,
-                _useNonce(owner),
-                deadline
-            )
-        );
-
-        bytes32 hash = _hashTypedDataV4(structHash);
-
-        address signer = ECDSA.recover(hash, v, r, s);
-        require(signer == owner, "ERC20Permit: invalid signature");
-
-        _approve(owner, spender, value);
-    }
-
-    /**
-     * @dev See {IERC20Permit-nonces}.
-     */
-    function nonces(address owner)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
-        return _nonces[owner].current();
-    }
-
-    /**
-     * @dev See {IERC20Permit-DOMAIN_SEPARATOR}.
-     */
-    // solhint-disable-next-line func-name-mixedcase
-    function DOMAIN_SEPARATOR() external view override returns (bytes32) {
-        return _domainSeparatorV4();
-    }
-
-    /**
-     * @dev "Consume a nonce": return the current value and increment.
-     *
-     * _Available since v4.1._
-     */
-    function _useNonce(address owner)
-        internal
-        virtual
-        returns (uint256 current)
-    {
-        Counters.Counter storage nonce = _nonces[owner];
-        current = nonce.current();
-        nonce.increment();
     }
 }
