e4ae595528c5d18b8431c37f7ce2813d9bc5b890
Adam H <surfcoderepeat@protonmail.com>
2021-09-20 09:55:07 -0600

    fix permit

--- contracts/Cudl.sol
+++ contracts/Cudl.sol
@@ -9,7 +9,72 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-abstract contract ERC20Permit is IERC20Permit, EIP712 {
+interface IArbToken {
+    /**
+     * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeMint(address account, uint256 amount) external;
+
+    /**
+     * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeBurn(address account, uint256 amount) external;
+
+    /**
+     * @return address of layer 1 token
+     */
+    function l1Address() external view returns (address);
+}
+
+contract Cudl is
+    IArbToken,
+    ERC20PresetMinterPauser("CUDL", "CUDL"),
+    IERC20Permit,
+    EIP712
+{
+    address public l2Gateway;
+    address public override l1Address;
+
+    modifier onlyGateway() {
+        require(msg.sender == l2Gateway, "ONLY_GATEWAY");
+        _;
+    }
+
+    constructor(address l2Gateway_, address l1Counterpart_)
+        EIP712("CUDL", "1")
+    {
+        require(l2Gateway_ != address(0), "INVALID_GATEWAY");
+        require(l2Gateway == address(0), "ALREADY_INIT");
+        l2Gateway = l2Gateway_;
+        l1Address = l1Counterpart_;
+    }
+
+    function bridgeMint(address account, uint256 amount)
+        external
+        virtual
+        override
+        onlyGateway
+    {
+        _mint(account, amount);
+    }
+
+    /**
+     * @notice Burn tokens on L2.
+     * @dev only the token bridge can call this
+     * @param account owner of tokens
+     * @param amount amount of tokens burnt
+     */
+    function bridgeBurn(address account, uint256 amount)
+        external
+        virtual
+        override
+        onlyGateway
+    {
+        _burn(account, amount);
+    }
+
+    // PERMIT
+
     using Counters for Counters.Counter;
 
     mapping(address => Counters.Counter) private _nonces;
@@ -25,7 +90,6 @@ abstract contract ERC20Permit is IERC20Permit, EIP712 {
      *
      * It's a good idea to use the same `name` that is defined as the ERC20 token name.
      */
-    constructor(string memory name) EIP712(name, "1") {}
 
     /**
      * @dev See {IERC20Permit-permit}.
@@ -94,67 +158,5 @@ abstract contract ERC20Permit is IERC20Permit, EIP712 {
         Counters.Counter storage nonce = _nonces[owner];
         current = nonce.current();
         nonce.increment();
-    }
-}
-
-interface IArbToken {
-    /**
-     * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
-     */
-    function bridgeMint(address account, uint256 amount) external;
-
-    /**
-     * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
-     */
-    function bridgeBurn(address account, uint256 amount) external;
-
-    /**
-     * @return address of layer 1 token
-     */
-    function l1Address() external view returns (address);
-}
-
-contract Cudl is
-    IArbToken,
-    ERC20PresetMinterPauser("CUDL", "CUDL"),
-    ERC20Permit("CUDL")
-{
-    address public l2Gateway;
-    address public override l1Address;
-
-    modifier onlyGateway() {
-        require(msg.sender == l2Gateway, "ONLY_GATEWAY");
-        _;
-    }
-
-    constructor(address l2Gateway_, address l1Counterpart_) {
-        require(l2Gateway_ != address(0), "INVALID_GATEWAY");
-        require(l2Gateway == address(0), "ALREADY_INIT");
-        l2Gateway = l2Gateway_;
-        l1Address = l1Counterpart_;
-    }
-
-    function bridgeMint(address account, uint256 amount)
-        external
-        virtual
-        override
-        onlyGateway
-    {
-        _mint(account, amount);
-    }
-
-    /**
-     * @notice Burn tokens on L2.
-     * @dev only the token bridge can call this
-     * @param account owner of tokens
-     * @param amount amount of tokens burnt
-     */
-    function bridgeBurn(address account, uint256 amount)
-        external
-        virtual
-        override
-        onlyGateway
-    {
-        _burn(account, amount);
     }
 }
